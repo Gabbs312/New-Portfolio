b4afa68170c26f891c7ed0678e7cfa29aa2a6f64
Gabby <165937271+Gabbs312@users.noreply.github.com>
2025-06-18 18:31:31 +0100

    Update!

--- script.js
+++ script.js
@@ -1,24 +1,4 @@
 
-const sections = document.querySelectorAll('.page-section');
-  let currentSectionIndex = 0;
-
-
-  function scrollToSection(index) {
-    if (index >= 0 && index < sections.length) {
-      sections[index].scrollIntoView({ behavior: 'smooth' });
-      currentSectionIndex = index;
-    }
-  }
-  document.querySelector('.harrow').addEventListener('click', () => {
-    scrollToSection(currentSectionIndex + 1);
-  });
-  document.querySelector('.arrow2').addEventListener('click', () => {
-    scrollToSection(currentSectionIndex + 1);
-  });
-
-  document.querySelector('.arrow1').addEventListener('click', () => {
-    scrollToSection(currentSectionIndex - 1);
-  });
 
   
 
